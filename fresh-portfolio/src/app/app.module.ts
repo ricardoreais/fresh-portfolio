6da218a11294d37416d7d126a09158f78b4750eb
Ricardo Reais <Ricardo.Reais@vortal.biz>
2019-08-11 20:45:31 +0100

    Initial commit

---
+++ fresh-portfolio/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { IntroComponent } from './intro/intro.component';
+import { BioComponent } from './bio/bio.component';
+import { TimelineComponent } from './timeline/timeline.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    IntroComponent,
+    BioComponent,
+    TimelineComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
