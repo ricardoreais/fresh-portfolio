c762d9a486fa74840aebe20cdc56c01e0e055949
Ricardo Reais <Ricardo.Reais@vortal.biz>
2019-09-04 00:02:38 +0100

    Added carousel pause on hover

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
+  pauseOnHover = true;
 
   constructor() { }
 
