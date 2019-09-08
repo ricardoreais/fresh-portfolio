9b3bf32e47f84e87719523c6daf6d88f41ae1bad
Ricardo Reais <Ricardo.Reais@vortal.biz>
2019-09-08 14:23:23 +0100

    Added projects url

--- src/app/models/project.model.ts
+++ src/app/models/project.model.ts
@@ -2,10 +2,12 @@ export class Project {
   name: string;
   description: string;
   imagePath: string;
+  url: string;
 
-  constructor(name: string, description: string, imagePath: string) {
+  constructor(name: string, description: string, imagePath: string, url?: string) {
     this.name = name;
     this.description = description;
     this.imagePath = imagePath;
+    this.url = url;
   }
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -8,7 +8,12 @@ import { Project } from '../models/project.model';
 })
 export class ProjectsComponent implements OnInit {
   projects: Project[] = [
-    new Project('Hangman game', 'A mini web app with the classic hangman game!', 'assets/images/hangman.png'),
+    new Project(
+      'Hangman game',
+      'A mini web app with the classic hangman game!',
+      'assets/images/hangman.png',
+      'https://ricardoreais.github.io/hangman/'
+    ),
     new Project('Keyword search', 'An autocomplete search with highlights!', 'assets/images/keyword-search.png')
   ];
 
